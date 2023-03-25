268dbfed805e25ada905d2b8522ad40a2aa9fd34
punkestu <pangestubima89@gmail.com>
2023-03-25 14:25:04 +0700

    create basic (and bloated) authentication + minimal authorization

--- controller/user.js
+++ controller/user.js
@@ -1,17 +1,48 @@
 const {user} = require('../prisma/db');
+const jwt = require('jsonwebtoken');
 
 module.exports = {
-	register: async function (req, res) {
-		const result = await user.create({
-			data: {
-				...req.body,
-				role: {
-					connect:{
-						role: "client"
-					}
+	login: async function (req,res,next){
+		try{
+			const User = await user.findFirst({
+				where: {
+					email: req.body.email
+				}
+			});
+
+			req.errors = {user: '', password:''};
+			if(User == null){
+				req.errors.user = 'User not found';
+			}else{
+				if(User.password == req.body.password){
+					req.token = jwt.sign(User, 'this_is_secret_key', {expiresIn: 60*60});
+				}else{
+					req.errors.password = 'Wrong Password';
 				}
 			}
-		});
-		return res.send(result);
+			return next();
+		}catch (e) {
+			console.log(e);
+			return res.sendStatus(400);
+		}
+	},
+	register: async function (req, res, next) {
+		try {
+			const result = await user.create({
+				data: {
+					...req.body,
+					role: {
+						connect: {
+							role: "client"
+						}
+					}
+				}
+			});
+			req.token = jwt.sign(result, 'this_is_secret_key', {expiresIn: 60*60});
+			next();
+		}catch (e) {
+			console.log(e);
+			return res.sendStatus(400);
+		}
 	}
 }
--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,30 @@ const express = require('express');
 const userController = require("../controller/user");
 const router = express.Router();
 
+function jsonToQueryString(json) {
+  return '?' +
+      Object.keys(json).map(function(key) {
+        return encodeURIComponent(key) + '=' +
+            encodeURIComponent(json[key]);
+      }).join('&');
+}
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/register', userController.register);
+router.post('/register', userController.register, function(req,res){
+  if(req.token){
+    return res.cookie("token",req.token, {maxAge: 1000*60*60}).redirect("/home");
+  }
+  return res.status(404).redirect('/register'+jsonToQueryString(req.errors));
+});
+
+router.post('/login', userController.login, function(req,res){
+  if(req.token){
+    return res.cookie("token", req.token, {maxAge: 1000*60*60}).redirect("/home");
+  }
+  return res.status(404).redirect('/login'+jsonToQueryString(req.errors));
+});
 
 module.exports = router;
