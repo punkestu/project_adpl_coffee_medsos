9810c36a40d2110b56477431b21d46e7f74eb04f
punkestu <pangestubima89@gmail.com>
2023-03-27 21:20:48 +0700

    menambahkan update controller

--- controller/kedai.js
+++ controller/kedai.js
@@ -1,11 +1,8 @@
 const {kedaiProfile} = require('../prisma/db');
-const {validateToken} = require('./user');
 
 module.exports = {
 	getKedai: async function (req, res, next) {
 		try {
-			// const User = await validateToken(req.cookies.token);
-			// if(User){
 			const kedai = await kedaiProfile.findFirst({
 				where: {
 					user: {
@@ -22,7 +19,6 @@ module.exports = {
 				}
 			});
 			req.kedai = kedai;
-			// }
 			next();
 		} catch (e) {
 			console.log(e);
@@ -31,12 +27,11 @@ module.exports = {
 	},
 	create: async function (req, res, next) {
 		try {
-			const User = await validateToken(req.cookies.token);
 			const result = await kedaiProfile.create({
 				data: {
 					user: {
 						connect: {
-							Id: User.Id
+							Id: req.user.Id
 						}
 					},
 					description: req.body.deskripsi,
@@ -54,6 +49,30 @@ module.exports = {
 		} catch (e) {
 			console.log(e);
 			return res.sendStatus(400);
+		}
+	},
+	update: async function (req,res,next) {
+		try{
+			const result = await kedaiProfile.update({
+				where: {
+					Id: req.user.Id
+				},
+				data: {
+					description: req.body.deskripsi,
+					KedaiAlamat: {
+						update: {
+							jalan: req.body.jalan,
+							kecamatan: req.body.kecamatan,
+							kota: req.body.kota
+						}
+					}
+				}
+			});
+			console.log(result);
+			next();
+		}catch (e) {
+			console.log(e);
+			return res.render("500");
 		}
 	}
 };
--- routes/kedais.js
+++ routes/kedais.js
@@ -1,19 +1,23 @@
 const express = require('express');
 const kedaiController = require("../controller/kedai");
 const router = express.Router();
+const midKedai = require("../middleware/kedai");
 
-router.get('/create', function (req,res){
-	return res.render('kedai_create', {errors: req.params});
-});
-
-router.get('/k/:user', kedaiController.getKedai, function(req,res){
+router.get('/p/:user', kedaiController.getKedai, function(req,res){
 	if(req.kedai){
 		return res.render('kedai_profile', {kedai: req.kedai});
 	}
 	return res.send("kedai tidak ada");
 });
 
-router.post('/create', kedaiController.create, function (req,res){
+router.post('/profile', midKedai.update, kedaiController.update, function (req,res){
+	if(!req.errors){
+		return res.redirect('/home');
+	}
+	return res.status(400).redirect('/home');
+});
+
+router.post('/', midKedai.create, kedaiController.create, function (req,res){
 	if(!req.errors){
 		return res.redirect('/home');
 	}
